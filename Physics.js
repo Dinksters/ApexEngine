82bff5524ceaf7061bfdef0adabc16564c39eb3f
Dinksters <booyah.2628@gmail.com>
2026-04-28 12:34:42 +0530

    Update Physics.js

--- Physics.js
+++ Physics.js
@@ -1,35 +1,19 @@
 import * as CANNON from 'cannon-es';
 
-export let world;
-export let groundMaterial;
-
-const TIME_STEP = 1 / 120; 
-const MAX_SUB_STEPS = 3;
+export let world, groundMaterial;
 
 export function initPhysics() {
-    world = new CANNON.World({
-        gravity: new CANNON.Vec3(0, -9.81, 0)
-    });
-
+    world = new CANNON.World({ gravity: new CANNON.Vec3(0, -9.81, 0) });
     world.broadphase = new CANNON.SAPBroadphase(world);
-    world.solver.iterations = 10;
     
     groundMaterial = new CANNON.Material("ground");
-    const defaultMaterial = new CANNON.Material("default");
-    
-    const defaultContactMaterial = new CANNON.ContactMaterial(groundMaterial, defaultMaterial, {
-        friction: 0.5,
-        restitution: 0.1
-    });
-    world.addContactMaterial(defaultContactMaterial);
+    const carMat = new CANNON.Material("car");
+    world.addContactMaterial(new CANNON.ContactMaterial(groundMaterial, carMat, { friction: 0.5, restitution: 0.1 }));
 
-    const groundBody = new CANNON.Body({ mass: 0, material: groundMaterial });
-    const groundShape = new CANNON.Plane();
-    groundBody.addShape(groundShape);
-    groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
-    world.addBody(groundBody);
+    const ground = new CANNON.Body({ mass: 0, material: groundMaterial });
+    ground.addShape(new CANNON.Plane());
+    ground.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+    world.addBody(ground);
 }
 
-export function stepPhysics(deltaTime) {
-    world.step(TIME_STEP, deltaTime, MAX_SUB_STEPS);
-}
+export function stepPhysics(dt) { world.step(1/120, dt, 3); }
